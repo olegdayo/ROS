a9901366858a3367d50a4c4948ff0d27ee6d8a99
Mmmr7 <ol-sidorenkov@mail.ru>
2023-02-25 15:59:20 +0300

    if2match

--- syscall/src/utils.rs
+++ syscall/src/utils.rs
@@ -14,20 +14,24 @@ macro_rules! c_strs {
             let mut res_vec = Vec::<String>::new();
             for elem in temp_vec {
                 if &elem == "." {
-                    if res_vec.len() == 0 {
-                        res_vec.push(elem);
-                        continue;
+                    match res_vec.last_mut() {
+                        Some(last) => {
+                            *last += &elem;
+                        }
+
+                        None => {
+                            res_vec.push(elem);
+                        }
                     }
 
-                    *res_vec.last_mut().unwrap() += &elem;
                     continue;
                 }
 
-                match res_vec.last() {
-                    Some(cmd) => {
-                        match *cmd.as_bytes().last().unwrap() as char {
+                match res_vec.last_mut() {
+                    Some(last) => {
+                        match *last.as_bytes().last().unwrap() as char {
                             '-' | '.' => {
-                                *res_vec.last_mut().unwrap() += &elem;
+                                *last += &elem;
                             }
                             _ => {
                                 res_vec.push(elem);
